cce2d5ec797b7cdd265d9fce45555fa8383a90fe
Jake O'Toole <otoolej33@gmail.com>
2020-12-23 09:44:10 -0600

    Created initial seed. Need to add types to seed.

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+USE pokemonDB;
+
+INSERT INTO pokemon 
+  (poke_name, type_one, type_two) 
+  VALUES
+  ('Bulbasaur', 1, 5),
+  ('Ivysaur', 1, 5),
+  ('Venusaur', 1, 5),
+  ('Charmander', 2, NULL),
+  ('Charmeleon', 2, NULL),
+  ('Charizard', 2, 4),
+  ('Squirtle', 3, NULL),
+  ('Wartortle', 3, NULL),
+  ('Blastoise', 3, NULL);
