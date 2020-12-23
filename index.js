fcd3903e784b6437ccf8243a59cd2db493f9b36f
Jake O'Toole <otoolej33@gmail.com>
2020-12-23 11:07:22 -0600

    Created function to view all types.

--- index.js
+++ index.js
@@ -19,13 +19,12 @@ function runPokedex() {
       name: "test",
       type: "rawlist",
       message: "Welcome to the Pokedex App!",
-      choices: ["test 1", "test 2", "exit"],
+      choices: ["View all types", "test 2", "exit"],
     })
     .then(function (answer) {
       switch (answer.test) {
-        case "test 1":
-          console.log("You choose test 1");
-          runPokedex();
+        case "View all types":
+          viewAllTypes();
           break;
         case "test 2":
           console.log("You chose Test 2");
@@ -48,10 +47,18 @@ function runPokedex() {
 }
 
 function viewAllTypes() {
-  let query = "SELECT * FROM pokemon";
+  // You could put the query inside the connection.query() call, but it makes it a little cleaner this way.
+  let query = "SELECT * FROM types";
   connection.query(query, function (err, res) {
     // handle outcome where an error has occurred.
     if (err) throw err;
+    // element is a parameter name chosen by me
+    // -- when using the method forEach, you have a callback function that gets one indice from the array, ...
+    //   ... in this case 'res' at a time. We are using the name "element" for indice.
+    //   One element from from the array is passed in each time.
+    res.forEach((element) => {
+      console.log(`Type ID: ${element.id} Name: ${element.type_name}`);
+    });
+    runPokedex();
   });
-  console.log();
 }
