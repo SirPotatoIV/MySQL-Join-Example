0c7176e21f653f955b609ce3534e818b57f9a799
Jake O'Toole <otoolej33@gmail.com>
2020-12-23 12:01:33 -0600

    Created new file to store all of my queries, which I used for testing out my queries while I was developing my code.

---
+++ db/queries.sql
@@ -0,0 +1,28 @@
+-- I created this file while testing my queries.
+-- I find it easier to test the queries in MySQL Workbench first. Then add them to my code.
+
+USE pokemondb;
+
+SELECT 
+	pokemon.id,
+    pokemon.poke_name,
+    -- type_1 is really table pokemon_types. I have set up an aliase further down.
+    -- the keyword AS makes it possible to change the column name. Instead of saying type_1 it will say primary_type
+    type_1.type_name AS primary_type,
+	-- type_2 is really table pokemon_types. I have set up an aliase further down.
+    -- the keyword AS makes it possible to change the column name. Instead of saying type_2 it will say secondary_type
+    type_2.type_name AS secondary_type
+FROM 
+	pokemon
+-- here is where I set up an aliase for table pokemon_types. I need this so I can join to the same table twice.
+LEFT JOIN pokemon_types AS type_1 ON 
+	pokemon.type_one = type_1.id
+LEFT JOIN pokemon_types AS type_2 ON 
+	pokemon.type_two = type_2.id;
+    
+SELECT * FROM pokemon;
+SELECT * FROM pokemon_types;
+
+-- Resources
+-- How to do a LEFT JOIN: https://www.mysqltutorial.org/mysql-left-join.aspx
+-- Joining to the same table twice: https://stackoverflow.com/questions/199953/how-do-you-join-on-the-same-table-twice-in-mysql
